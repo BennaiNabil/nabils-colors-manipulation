be969ab8406e6183f9fe6cd27f058863c2e52309
Bennai Nabil <40771404+NabilBennai@users.noreply.github.com>
2021-08-18 01:43:29 +0200

    ColorGenerator component placed and stylized. TODO: color generation and its emission to its child

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,32 @@
 <template>
   <v-app>
     <Navbar></Navbar>
+    <v-main>
+      <div class="my-11">
+        <div class="row">
+          <div class="col-4">
+            <ColorGenerator></ColorGenerator>
+          </div>
+          <div class="col-8">
+            <div class="row">
+              <div class="col-6">Des dingueries</div>
+              <div class="col-6">Des nouvelles dingueries</div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </v-main>
   </v-app>
 </template>
 <script>
 
 import Navbar from "./components/Navbar";
+import ColorGenerator from "@/components/ColorGenerator";
 
 export default {
   name: 'App',
 
-  components: {Navbar},
+  components: {ColorGenerator, Navbar},
 
   data: () => ({
     //
---
+++ src/components/ColorGenerator.vue
@@ -0,0 +1,44 @@
+<template>
+  <div>
+    <v-card class="mx-auto nab-colgen-maincard text-center"
+            max-width="344"
+            outlined>
+      <v-card-subtitle>
+        Bored of this card color ?
+      </v-card-subtitle>
+      <v-card-subtitle>
+        Click <v-icon
+          large
+          color="blue-grey darken-2"
+      >
+        mdi-chevron-down
+      </v-icon>
+        <span class="font-weight-bold">below</span> to change its color!
+      </v-card-subtitle>
+      <v-card-subtitle>
+        <v-btn class="nab-colgen-button text--indigo">Change color</v-btn>
+      </v-card-subtitle>
+    </v-card>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "ColorGenerator"
+}
+</script>
+
+<style>
+.nab-colgen-button {
+  background: #FFFFFF !important;
+  border: 1px solid #CACACA !important;
+  box-sizing: border-box !important;
+  border-radius: 8px !important;
+  padding: 0.7rem;
+  color: #9ECBFF !important;
+}
+
+.nab-colgen-maincard {
+  background-color: #9ECBFF !important;
+}
+</style>

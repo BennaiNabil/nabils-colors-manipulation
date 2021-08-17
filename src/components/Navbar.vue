9da718885990f8d68e69c0e616778483ffeb423c
Bennai Nabil <40771404+NabilBennai@users.noreply.github.com>
2021-08-18 01:16:57 +0200

    Navbar component integrated as of the mockup

---
+++ src/components/Navbar.vue
@@ -0,0 +1,32 @@
+<template>
+  <v-app-bar app class="nab-navbar text-no-wrap" style="flex: 1 1;">
+    <v-app-bar-title>Random background generator V3</v-app-bar-title>
+    <v-spacer></v-spacer>
+    <span>
+      <a class="nab-nav-link" href="#">SEE MORE</a>
+    </span>
+  </v-app-bar>
+</template>
+
+<script>
+export default {
+  name: "Navbar"
+}
+</script>
+
+<style scoped>
+.nab-navbar {
+  background: linear-gradient(90deg, #DCB8CB 0%, #D5C9DF 53.65%, #DDF1FB 100%) !important;
+  color: #FFFFFF !important;
+
+}
+
+.nab-nav-link {
+  color: #808080;
+  text-decoration: inherit;
+}
+
+.v-app-bar-title__content{
+  width: 200px !important;
+}
+</style>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,20 @@
 import Vue from 'vue'
 import App from './App.vue'
+import vuetify from './plugins/vuetify'
+import colors from 'vuetify/lib/util/colors'
 
 Vue.config.productionTip = false
 
 new Vue({
-  render: h => h(App),
+    vuetify,
+    theme: {
+        themes: {
+            light: {
+                primary: colors.red.darken1, // #E53935
+                secondary: colors.red.lighten4, // #FFCDD2
+                accent: colors.indigo.base, // #3F51B5
+            },
+        },
+    },
+    render: h => h(App)
 }).$mount('#app')

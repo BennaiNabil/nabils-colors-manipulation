ad8f794a7c21cb53f67f5eaff8849d8eb0646cc2
Bennai Nabil <40771404+NabilBennai@users.noreply.github.com>
2021-08-18 02:23:46 +0200

    New color generated on click and contrast issues dealt with for the most part. TODO: Emit upwards the color to continue integrating the mockup

--- src/components/ColorGenerator.vue
+++ src/components/ColorGenerator.vue
@@ -1,22 +1,24 @@
 <template>
   <div>
-    <v-card class="mx-auto nab-colgen-maincard text-center"
+    <v-card :style="{backgroundColor: ''+newColor}"
+            class="mx-auto text-center"
             max-width="344"
             outlined>
-      <v-card-subtitle>
+
+      <v-card-subtitle :style="{color: `${isColorLight(newColor) ? '#000' : '#fff'}`    }">
         Bored of this card color ?
       </v-card-subtitle>
-      <v-card-subtitle>
-        Click <v-icon
-          large
-          color="blue-grey darken-2"
-      >
-        mdi-chevron-down
-      </v-icon>
+      <v-card-subtitle :style="{color: `${isColorLight(newColor) ? '#000' : '#fff'}`}">
+        Click
+        <v-icon
+            :color="isColorLight(newColor)? 'black':'white'"
+            large>
+          mdi-chevron-down
+        </v-icon>
         <span class="font-weight-bold">below</span> to change its color!
       </v-card-subtitle>
       <v-card-subtitle>
-        <v-btn class="nab-colgen-button text--indigo">Change color</v-btn>
+        <v-btn class="nab-colgen-button" @click="genColor()">Change color {{ newColor }}</v-btn>
       </v-card-subtitle>
     </v-card>
   </div>
@@ -24,18 +26,37 @@
 
 <script>
 export default {
-  name: "ColorGenerator"
+  name: "ColorGenerator",
+  data: function () {
+    return {
+      newColor: '#000'
+    }
+  },
+  methods: {
+    genColor: function () {
+      this.newColor = `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+    },
+    isColorLight: function (color) {
+      const hexadecimalValue = color.replace("#", "");
+      const redComponent = parseInt(hexadecimalValue.substr(0, 2), 16);
+      const greenComponent = parseInt(hexadecimalValue.substr(2, 2), 16);
+      const blueComponent = parseInt(hexadecimalValue.substr(4, 2), 16);
+      const computedBrightness = (redComponent * 299 + greenComponent * 587 + blueComponent * 114) / 1000;
+      console.log(computedBrightness > 155 ? "Is bright" : "Is dark")
+      return computedBrightness > 155;
+    },
+  }
 }
 </script>
 
 <style>
 .nab-colgen-button {
-  background: #FFFFFF !important;
-  border: 1px solid #CACACA !important;
-  box-sizing: border-box !important;
-  border-radius: 8px !important;
+  background: #FFFFFF;
+  border: 1px solid #CACACA;
+  box-sizing: border-box;
+  border-radius: 8px;
   padding: 0.7rem;
-  color: #9ECBFF !important;
+  color: #9ECBFF;
 }
 
 .nab-colgen-maincard {
